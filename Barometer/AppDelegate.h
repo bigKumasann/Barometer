7f4154bcc06a0591e825a435523f4a90ffae5836
bigKumasann <limengqiang@wangleideiMac.local>
2015-08-05 11:31:13 +0800

    init commit

---
+++ Barometer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Barometer
+//
+//  Created by limengqiang on 15/8/5.
+//  Copyright (c) 2015年 wxdjg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Barometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barometer
+//
+//  Created by limengqiang on 15/8/5.
+//  Copyright (c) 2015年 wxdjg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
